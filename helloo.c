b3eba926c74db46b5dbf15083f53fc120d7f98ff
shoyab khan <129213723+mrshoya05@users.noreply.github.com>
2023-09-20 10:58:03 +0530

    Add files via upload
    switch conditions check with 07 days.

---
+++ helloo.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{ 
+   int day;
+   printf("enter no ");
+   scanf("%d",& day);
+      switch (day){
+         case 1 : printf("monday");
+         break;
+         case 2 : printf("Tuesday");
+         break;
+         case 3 : printf("wednesday");
+         break;
+         case 4 : printf("Thursday");
+         break;
+         case 5 : printf("Friday");
+         break;
+         case 6 : printf("Saturday");
+         break;
+         case 7 : printf("Sunday");
+         break;
+         default : printf("not a valid day");
+      }
+   return 0;
+}
